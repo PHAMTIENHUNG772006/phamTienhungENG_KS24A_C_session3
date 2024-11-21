040db5268504e3b1f5dfcf3c0c5088e377d49b5d
PHAMTIENHUNG772006 <hungpham772006@gmail.com>
2024-11-21 22:42:21 +0700

    Add files via upload

--- session3_Bt5.c
+++ session3_Bt5.c
@@ -4,13 +4,13 @@ int main(){
 	printf("STT   |  HO VA TEN   |  TUOI  |    SDT      |    EMAI\n");
 	printf("1       Nguyen van A    20      090448881     anv@rikkeiacademi.com\n");
 	printf("2       Nguyen van B    21      090448882     bnv@rikkeiacademi.com\n");
-    printf("3       Nguyen van C    20      090448883     cnv@rikkeiacademi.com\n");
-    printf("4       Nguyen van D    20      090448884     dnv@rikkeiacademi.com\n");
-    printf("5       Nguyen van E    20      090448885     env@rikkeiacademi.com\n");
-    printf("6       Nguyen van F    20      090448886     fnv@rikkeiacademi.com\n");
-    printf("7       Nguyen van G    20      090448887     gnv@rikkeiacademi.com\n");
-    printf("8       Nguyen van H    20      090448888     hnv@rikkeiacademi.com\n");
-    printf("9       Nguyen van I    20      090448889     inv@rikkeiacademi.com\n");
-    printf("10      Nguyen van K    20      090448880     knv@rikkeiacademi.com\n");
+    printf("3       Nguyen van C    18      090448883     cnv@rikkeiacademi.com\n");
+    printf("4       Nguyen van D    19      090448884     dnv@rikkeiacademi.com\n");
+    printf("5       Nguyen van E    22      090448885     env@rikkeiacademi.com\n");
+    printf("6       Nguyen van F    21      090448886     fnv@rikkeiacademi.com\n");
+    printf("7       Nguyen van G    23      090448887     gnv@rikkeiacademi.com\n");
+    printf("8       Nguyen van H    19      090448888     hnv@rikkeiacademi.com\n");
+    printf("9       Nguyen van I    18      090448889     inv@rikkeiacademi.com\n");
+    printf("10      Nguyen van K    17      090448880     knv@rikkeiacademi.com\n");
 	return 0;
 }
